85fa895a9541091e6650242ca32c661e2b35830f
jerry zhang <171250022@smail.nju.edu.cn>
2019-05-26 11:53:50 +0800

    'userHome'

--- theThird/src/main/resources/static/js/userHome.js
+++ theThird/src/main/resources/static/js/userHome.js
@@ -10,32 +10,37 @@ $(document).ready(function() {
             '/statistics/boxOffice/total',
             function (res) {
                 var data = res.content || [];
-                console.log(data)
+                console.log(data);
                 nameList = data.map(function (item) {
                     return item.name;
                 });
                 console.log(nameList);
+                var boxingStr='';
+                console.log(nameList.length);
+                for(var i=0;i<nameList.length;i++){
+                    if(i==0||i==1||i==2){
+                        boxingStr=boxingStr+'<div class="statistic-item">'+
+                            '<span class="icon-fire">'+nameList[i]+'</span>'+
+                            '<span class="badge">'+(i+1).toString()+'</span>'+
+                            '</div>'
+                    }
+                    else {
+                        boxingStr = boxingStr + '<div class="statistic-item">' +
+                            '<span class="icon-thumbs-up">' + nameList[i] + '</span>' +
+                            '<span class="error-text">' + (i+1).toString() + '</span>' +
+                            '</div>'
+                    }
+                }
+                console.log(boxingStr);
+                $("#boxingOffice").append(boxingStr);
             },
             function (error) {
                 alert(JSON.stringify(error));
             });
         //
-
-        console.log(nameList);
-        var boxingStr='';
-        console.log(nameList.length);
-        for(var i=0;i<nameList.length;i++){
-            boxingStr=boxingStr+'<div class="statistic-item">'+
-                '<span>'+setTimeout(nameList[i],3)+'</span>'+
-                '<span class="error-text">'+1+'</span>'+
-                '</div>'
-            console.log(boxingStr);
-        }
-        console.log(boxingStr);
-        $("#boxingOffice").append(boxingStr);
     }
     function getPopularMovie(){
         var num=0;
         console.log(num);
     }
-})
+});
