f40a266394e05019a7845fe19039a5f350d58b65
UsoRua <msssang233@outlook.com>
2019-06-02 21:20:32 +0800

    fea($location) 添加coupon 无门槛优惠券相关方法 包括获取,更改无门槛优惠券金额,为用户发放无门槛优惠券

--- theThird/src/main/java/com/example/cinema/controller/promotion/CouponController.java
+++ theThird/src/main/java/com/example/cinema/controller/promotion/CouponController.java
@@ -3,10 +3,10 @@ package com.example.cinema.controller.promotion;
 import com.example.cinema.bl.promotion.CouponService;
 import com.example.cinema.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 /**
  * Created by liying on 2019/4/16.
@@ -23,6 +23,45 @@ public class CouponController {
         return couponService.getCouponsByUser(userId);
     }
 
+    /**
+     * 获取无门槛优惠券的优惠金额
+     * @return
+     */
+    @GetMapping("coupon/getNum")
+    public ResponseVO getSpecialCoupon(){
+        return couponService.getSpecialCoupon();
+    }
+
+    /**
+     * 更改无门槛优惠券的优惠金额
+     * @param discount
+     * @return
+     */
+    @PostMapping("coupon/change")
+    public ResponseVO updateSpecialCouponDiscount(@PathVariable double discount){
+        return couponService.updateSpecialCouponDiscount(discount);
+    }
+
+    /**
+     *
+     * @param consume
+     * @return
+     */
+
+    @GetMapping("/coupon/allMember")
+    public ResponseVO getAllUserByConsume(@PathVariable double consume){
+        return couponService.getAllUserByConsume(consume);
+    }
+
+    /**
+     * 为用户方法无门槛优惠券
+     * @param userId
+     * @return
+     */
+    @PostMapping("/coupon/send")
+    public ResponseVO issueCoupon(@RequestParam List<Integer> userId){
+        return couponService.issueCoupon(userId);
+    }
 
 
 }
