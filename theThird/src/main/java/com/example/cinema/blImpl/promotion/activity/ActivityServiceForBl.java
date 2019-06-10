79daf8c499c01222f357e15eff3ada2f153c1479
UsoRua <msssang233@outlook.com>
2019-06-10 17:12:12 +0800

    refactor(#location)

--- theThird/src/main/java/com/example/cinema/blImpl/promotion/activity/ActivityServiceForBl.java
+++ theThird/src/main/java/com/example/cinema/blImpl/promotion/activity/ActivityServiceForBl.java
@@ -3,6 +3,7 @@ package com.example.cinema.blImpl.promotion.activity;
 import java.util.List;
 
 import com.example.cinema.po.Activity;
+import com.example.cinema.po.Coupon;
 
 public interface ActivityServiceForBl {
 	
@@ -12,4 +13,10 @@ public interface ActivityServiceForBl {
 	 * @return
 	 */
 	List<Activity> getActivitiesByMovieId(Integer movieId);
+
+
+	/**
+	 * 通过CouponId获取对应的Activity
+	 */
+	Activity getActivityByCouponId(Integer couponId);
 }
